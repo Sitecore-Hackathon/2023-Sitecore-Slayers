874bc3df6a19dfb4647ce31a4ff522d600565066
PerficientDave <Dave.Ambrose@Perficient.com>
2023-03-04 00:12:11 -0500

    Adding base Docker image

---
+++ src/sxastarter/src/pages/api/sitemap.ts
@@ -0,0 +1,68 @@
+import { AxiosResponse } from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getPublicUrl } from '@sitecore-jss/sitecore-jss-nextjs';
+import config from 'temp/config';
+import { AxiosDataFetcher, GraphQLSitemapXmlService } from '@sitecore-jss/sitecore-jss-nextjs';
+
+const ABSOLUTE_URL_REGEXP = '^(?:[a-z]+:)?//';
+
+const sitemapApi = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<NextApiResponse | void> => {
+  const {
+    query: { id },
+  } = req;
+  // create sitemap graphql service
+  const sitemapXmlService = new GraphQLSitemapXmlService({
+    endpoint: config.graphQLEndpoint,
+    apiKey: config.sitecoreApiKey,
+    siteName: config.jssAppName,
+  });
+
+  // if url has sitemap-{n}.xml type. The id - can be null if it's sitemap.xml request
+  const sitemapPath = await sitemapXmlService.getSitemap(id as string);
+
+  // if sitemap is match otherwise redirect to 404 page
+  if (sitemapPath) {
+    const isAbsoluteUrl = sitemapPath.match(ABSOLUTE_URL_REGEXP);
+    const sitemapUrl = isAbsoluteUrl ? sitemapPath : `${config.sitecoreApiHost}${sitemapPath}`;
+    res.setHeader('Content-Type', 'text/xml;charset=utf-8');
+
+    // need to prepare stream from sitemap url
+    return new AxiosDataFetcher()
+      .get(sitemapUrl, {
+        responseType: 'stream',
+      })
+      .then((response: AxiosResponse) => {
+        response.data.pipe(res);
+      })
+      .catch(() => res.redirect('/404'));
+  }
+
+  // this approache if user go to /sitemap.xml - under it generate xml page with list of sitemaps
+  const sitemaps = await sitemapXmlService.fetchSitemaps();
+
+  if (!sitemaps.length) {
+    return res.redirect('/404');
+  }
+
+  const SitemapLinks = sitemaps
+    .map((item) => {
+      const parseUrl = item.split('/');
+      const lastSegment = parseUrl[parseUrl.length - 1];
+
+      return `<sitemap>
+        <loc>${getPublicUrl()}/${lastSegment}</loc>
+      </sitemap>`;
+    })
+    .join('');
+
+  res.setHeader('Content-Type', 'text/xml;charset=utf-8');
+
+  return res.send(`
+  <sitemapindex xmlns="http://sitemaps.org/schemas/sitemap/0.9" encoding="UTF-8">${SitemapLinks}</sitemapindex>
+  `);
+};
+
+export default sitemapApi;
